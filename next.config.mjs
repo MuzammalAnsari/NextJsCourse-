475bb8c99159035d822e75a2ccb973b8e22f6a1f
MuzammalAnsari <muzammalbash@gmail.com>
2024-02-03 16:00:43 +0500

    created home,about and contact pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //this how you use external images
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
