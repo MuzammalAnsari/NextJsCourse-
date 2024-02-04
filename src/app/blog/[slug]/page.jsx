a9ec5cca9ecf917a12048d0af377f04003a947e0
MuzammalAnsari <muzammalbash@gmail.com>
2024-02-04 10:57:35 +0500

    frontend is completed except the auth

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,6 +1,38 @@
+import Image from "next/image"
+import styles from "./singlePost.module.css"
+
 function SinglePostPage() {
     return (
-        <div>SinglePostPage</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image
+                    src="https://images.pexels.com/photos/19137460/pexels-photo-19137460/free-photo-of-turquoise-sea-off-the-coast-of-bali.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    className={styles.img}
+                    fill
+                />
+            </div>
+
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>Title</h1>
+                <div className={styles.detail}>
+                    <Image
+                        className={styles.avatar}
+                        src="https://images.pexels.com/photos/19137460/pexels-photo-19137460/free-photo-of-turquoise-sea-off-the-coast-of-bali.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        width={50}
+                        height={50}
+                    />
+                    <div className={styles.detailText}>
+                        <div className={styles.detailTitle}>author</div>
+                        <div className={styles.detailValue}>jeffrey</div>
+                    </div>
+                    <div className={styles.detailText}>
+                        <div className={styles.detailTitle}>publish</div>
+                        <div className={styles.detailValue}>12345</div>
+                    </div>
+                </div>
+                <div className={styles.content}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa deleniti fuga aliquam vel laudantium. Reprehenderit laborum, unde reiciendis quia quo neque quos voluptatibus, nihil impedit veritatis sunt quaerat ab odio!</div>
+            </div>
+        </div>
     )
 }
 
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -4,17 +4,30 @@ import Link from "next/link"
 
 const PostCard = ({ post }) => {
     return (
+        // <div className={styles.container}>
+        //     <div className={styles.top}>
+        //         {post.img && <div className={styles.imgContainer}>
+        //             <Image src={post.img} alt="" fill className={styles.img} />
+        //         </div>}
+        //         <span className={styles.date}>{post.createdAt?.toString().slice(4, 16)}</span>
+        //     </div>
+        //     <div className={styles.bottom}>
+        //         <h1 className={styles.title}>{post.title}</h1>
+        //         <p className={styles.desc}>{post.body}</p>
+        //         <Link className={styles.link} href={`/blog/${post.slug}`}>READ MORE</Link>
+        //     </div>
+        // </div>
         <div className={styles.container}>
             <div className={styles.top}>
-                {post.img && <div className={styles.imgContainer}>
-                    <Image src={post.img} alt="" fill className={styles.img} />
-                </div>}
-                <span className={styles.date}>{post.createdAt?.toString().slice(4, 16)}</span>
+                <div className={styles.imgContainer}>
+                    <Image src="https://images.pexels.com/photos/19137460/pexels-photo-19137460/free-photo-of-turquoise-sea-off-the-coast-of-bali.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" fill className={styles.img} />
+                </div>
+                <span className={styles.date}>12345</span>
             </div>
             <div className={styles.bottom}>
-                <h1 className={styles.title}>{post.title}</h1>
-                <p className={styles.desc}>{post.body}</p>
-                <Link className={styles.link} href={`/blog/${post.slug}`}>READ MORE</Link>
+                <h1 className={styles.title}>title</h1>
+                <p className={styles.dec}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam quibusdam nemo consectetur quidem exercitationem ipsam illo eveniet. Sed a eos sapiente excepturi ullam aperiam alias. Ut vel doloribus repellat placeat!</p>
+                <Link href="/blog/post" className={styles.link}>READ MORE</Link>
             </div>
         </div>
     )
